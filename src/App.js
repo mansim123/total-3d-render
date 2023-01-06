13e4f9240e37c48b9d946e7444c3fc207d7c1e33
manuel yemoh <manuelyemoh@gmail.com>
2023-01-06 09:53:03 +0000

    added a whole new scene

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useRef } from "react";
 import { Canvas } from "@react-three/fiber";
 import "./style.css";
-import { PresentationControls, Sky } from "@react-three/drei";
+import { Environment, OrbitControls, useHelper } from "@react-three/drei";
 import { Ground } from "./Ground";
-import { Neck } from "./Neck";
-import { Blade } from "./Blade";
+import { useControls, folder } from "leva";
+import { DirectionalLightHelper, SpotLightHelper } from "three";
+// import { Neck } from "./Neck";
+// import { Blade } from "./Blade";
 // import { Roundhouse } from "./Roundhouse";
 // import { RockBaked } from "./RockBaked";
 // import { Dog } from "./Dog";
@@ -12,11 +14,64 @@ import { Blade } from "./Blade";
 // import { Rock } from "./Rock";
 // import { TreeReflect } from "./TreeReflect";
 // import { TreeTexture } from "./TreeTexture";
-import { BallSticks } from "./BallSticks";
+// import { BallSticks } from "./BallSticks";
 // import Ocean from "./Ocean";
+import { WindMetal } from "./WindMetal";
 import { Html, useProgress } from "@react-three/drei";
 
-function WindShow() {
+function ShowScene() {
+  const light = useRef();
+  const SpotlightRef = useRef();
+  const SpotlightRef2 = useRef();
+  useHelper(SpotlightRef, SpotLightHelper, "cyan");
+  useHelper(SpotlightRef2, SpotLightHelper, "blue");
+  useHelper(light, DirectionalLightHelper, 2, "red");
+
+  const { SpotLightIntensity, sx, sdis, scolor } = useControls({
+    SpotLight: folder({
+      SpotLightIntensity: { value: 5, min: 0, max: 50 },
+      sx: { value: 1, min: -10, max: 20 },
+      sdis: { value: 10, min: 0, max: 20 },
+      scolor: "#ff0000",
+    }),
+  });
+
+  const { SpotLightIntensity2, sx2, sdis2, scolor2 } = useControls({
+    SpotLight2: folder({
+      SpotLightIntensity2: { value: 5, min: 0, max: 50 },
+      sx2: { value: 1, min: -20, max: 20 },
+      sdis2: { value: 10, min: 0, max: 20 },
+      scolor2: "#0000ff",
+    }),
+  });
+
+  const { intensity, x } = useControls({
+    DirectionalLight: folder({
+      intensity: { value: 1, min: 0, max: 25 },
+      x: { value: 1, min: -10, max: 10 },
+    }),
+  });
+
+  const { PointLightIntensity, py, px, pz, pcolor } = useControls({
+    PointLight: folder({
+      PointLightIntensity: { value: 1, min: 0, max: 25 },
+      px: { value: 1, min: -10, max: 10 },
+      py: { value: 1, min: -10, max: 10 },
+      pz: { value: 1, min: -10, max: 10 },
+      pcolor: "#fff",
+    }),
+  });
+
+  const { scale } = useControls("Wind Scale", {
+    windControls: folder({
+      scale: {
+        value: 1,
+        min: 1,
+        max: 10,
+      },
+    }),
+  });
+
   return (
     <>
       {/* <OrbitControls target={[0, 1.75, 0]} maxPolarAngle={1.45} /> */}
@@ -25,7 +80,7 @@ function WindShow() {
 
       {/* <Neck />
       <Blade /> */}
-      <BallSticks />
+      {/* <BallSticks /> */}
       {/* <Dog /> */}
       {/* <Roundhouse /> */}
       {/* <RockBaked /> */}
@@ -33,7 +88,9 @@ function WindShow() {
       {/* <Rock /> */}
       {/* <TreeReflect />
 
+
       <TreeTexture /> */}
+      <WindMetal scale={scale} />
 
       {/* <mesh>
         <boxGeometry args={[1, 1, 1]} />
@@ -41,23 +98,35 @@ function WindShow() {
       </mesh> */}
 
       <spotLight
-        color={[1, 0.4, 1]}
-        intensity={2}
-        angle={2}
+        ref={SpotlightRef}
+        color={scolor}
+        intensity={SpotLightIntensity}
+        angle={150}
+        distance={sdis}
         penumbra={0.5}
-        position={[10, 5, 10]}
+        position={[sx, 5, 3]}
         castShadow
         shadow-bias={-0.0001}
       />
-
       <spotLight
-        color={[1, 1, 1]}
-        intensity={1}
-        angle={2}
+        ref={SpotlightRef2}
+        color={scolor2}
+        intensity={SpotLightIntensity2}
+        angle={150}
+        distance={sdis2}
         penumbra={0.5}
-        position={[15, 10, -15]}
+        position={[sx2, 5, -3]}
         castShadow
         shadow-bias={-0.0001}
+      />
+      {/* <ambientLight intensity={intensity} /> */}
+      {/* <hemisphereLight args={["#fff", "#333", intensity]} /> */}
+      <directionalLight ref={light} position={[x, 5, 0]} intensity={intensity} />
+      <pointLight
+        color={pcolor}
+        position={[px, py, pz]}
+        castShadow
+        intensity={PointLightIntensity}
       />
 
       <Ground />
@@ -77,20 +146,32 @@ function App() {
         <h1 id="number">0</h1>
       </section>
 
-      {/* <Canvas id="Canvas" shadows camera={{ position: [0, 0, 10], fov: 50 }}> */}
-      <Canvas id="Canvas" shadows>
+      <Canvas id="Canvas" shadows camera={{ position: [0, 0, 10], fov: 50 }}>
+        {/* <Canvas id="Canvas" shadows> */}
         <Suspense fallback={<Loader />}>
-          <PresentationControls
+          {/* <PresentationControls
             global
             config={{ mass: 1, tension: 170, friction: 26 }}
             speed={2}
             rotation={[0.1, 0, 0]}
             polar={[0, Math.PI / 2]}
             azimuth={[-Infinity, Infinity]}
-          >
-            <WindShow />
-            <Sky scale={1000} sunPosition={[500, 150, -1000]} turbidity={0.1} />
-          </PresentationControls>
+            zoom={-10}
+          > */}
+          <ShowScene />
+          {/* <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade speed={1} /> */}
+          {/* <Sky scale={1000} sunPosition={[500, 150, -1000]} turbidity={0.1} /> */}
+          <Environment
+            files="https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/2k/evening_road_01_2k.hdr"
+            ground={{ height: 5, radius: 40, scale: 20 }}
+          />
+          <OrbitControls
+            enableZoom={true}
+            makeDefault
+            minPolarAngle={0}
+            maxPolarAngle={Math.PI / 2.25}
+          />
+          {/* </PresentationControls> */}
           {/* <Ocean /> */}
         </Suspense>
       </Canvas>
--- src/WindMetal.js
+++ src/WindMetal.js
@@ -1,28 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { Mesh } from "three";
 
-export function House() {
-  const gltf = useLoader(GLTFLoader, process.env.PUBLIC_URL + "models/house/house.gltf");
-
+export function WindMetal(props) {
+  const gltf = useLoader(GLTFLoader, process.env.PUBLIC_URL + "models/windmill/wind-metal.gltf");
+  //console.log(props);
   //const crimson = new THREE.Color(0xcccccc);
   //const [currentColor, setCurrentColor] = useState(crimson);
 
+  const newScale = props.scale / 1000;
+
+  const [myScale, setMyScale] = useState(newScale);
+
   useEffect(() => {
     //setCurrentColor(crimson);
+    console.log(gltf);
     gltf.scene.scale.set(0.01, 0.01, 0.01);
-    gltf.scene.position.set(0, 0.75, 0);
+    gltf.scene.position.set(0, 2.8, 0);
     gltf.scene.traverse((object) => {
       if (object instanceof Mesh) {
-        // object.castShadow = true;
-        // object.receiveShadow = true;
+        object.castShadow = true;
+        object.receiveShadow = true;
         object.material.envMapIntensity = 1;
         //object.material.color = currentColor;
       }
     });
-  }, [gltf]);
 
+    setMyScale(newScale);
+    gltf.scene.scale.set(myScale, myScale, myScale);
+  }, [gltf, props, myScale, newScale]);
+
+  //console.log((gltf.scene.scale.x = props.value));
   //   useFrame((state, delta) => {
   //     let t = state.clock.getElapsedTime();
 
